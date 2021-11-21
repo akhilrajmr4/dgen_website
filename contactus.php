3bc1b49daa5f47c14226413f4eb80bb7c778f83f
akhilrajmr4 <akhi.akhilraj18@gmail.com>
2021-11-21 11:17:35 +0530

    d gen new changes

--- contactus.php
+++ contactus.php
@@ -9,7 +9,7 @@
 		$email = $_POST["email"];
 		$subject = $_POST["subject"];
 		$message1 = $_POST["message"];
-		$message ="Name : ".$Name."\nEmail : "$email."\nSubject : ".$subject."\nMessage: ".$message1;
+		$message ="Name : ".$Name."\nEmail : ".$email."\nSubject : ".$subject."\nMessage: ".$message1;
 		// a random hash will be necessary to send mixed content
 		$separator = md5(time());
 
@@ -17,17 +17,12 @@
 		$eol = "\r\n";
 
 		// main header (multipart mandatory)
-		$headers = "From: name <test@test.com>" . $eol;
+		$headers = "From: name <Dgen@website.com>" . $eol;
 		$headers .= "MIME-Version: 1.0" . $eol;
 		$headers .= "Content-Type: multipart/mixed; boundary=\"" . $separator . "\"" . $eol;
 		$headers .= "Content-Transfer-Encoding: 7bit" . $eol;
-		$headers .= "This is a MIME encoded message." . $eol;
+		$headers .= "This Message From Dgen website." . $eol;
 
-		// message
-		$body = "--" . $separator . $eol;
-		$body .= "Content-Type: text/plain; charset=\"iso-8859-1\"" . $eol;
-		$body .= "Content-Transfer-Encoding: 8bit" . $eol;
-		$body .= $message . $eol;
 
 
 		if(mail($to, $subject, $message, $headers))
